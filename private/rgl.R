cad881a0bb7f39089209e4561cddd15570e869d3
Jacek Pardyak <jacek.pardyak@nl.team.blue>
2022-09-20 16:29:19 +0200

    ssss

--- private/rgl.R
+++ private/rgl.R
@@ -1,15 +1,4 @@
 library(rgl)
-open3d()
-shade3d( translate3d( cube3d(col = "green"), 0, 0, 0) )
-shade3d( translate3d( cube3d(col = "#FFFFFF"), 2, 0, 0) )
-
-#strtoi(
-x =  as.hexmode( 16)
-y =  as.hexmode(120)
-
-x = 255 %>% as.integer() %>% as.hexmode()
-y = 122 %>% as.integer() %>% as.hexmode()
-z = 78 %>% as.integer() %>% as.hexmode()
 
 by = 50
 sequence <- seq(from = 0, to = 255, by = by)
@@ -31,20 +20,34 @@ for(x in sequence){
 }
 
 
+# case z == min(sequence) | z == max(sequence)
+# for x in range, y in range draw
+# case z != min(sequence) & z != max(sequence)
+# y == min(sequence) | y == max(sequence)
+# for x in range, z in range draw(x,y,z)
+
 open3d()
 for(x in sequence){
   for (y in sequence) {
     for (z in sequence) {
-      if(x > min(seq) & x < max(seq))
+      if(z == min(sequence) | z == max(sequence)){
       colour = c(x, y, z) %>% 
         as.hexmode() %>% 
         format(width = 2) %>% 
         paste(collapse = "") %>%
         paste0("#", .)
       print(colour)
-#      shade3d( translate3d( cube3d(col = colour), x/by, y/by, z/by) )
+      shade3d( translate3d( cube3d(col = colour), 2*x/by, 2*y/by, 2*z/by) )
     }
-    
+    if(x == min(sequence) | x == max(sequence)){
+      colour = c(x, y, z) %>% 
+        as.hexmode() %>% 
+        format(width = 2) %>% 
+        paste(collapse = "") %>%
+        paste0("#", .)
+      print(colour)
+      shade3d( translate3d( cube3d(col = colour), 2*x/by, 2*y/by, 2*z/by) )
+    }}
   }
 }
 
